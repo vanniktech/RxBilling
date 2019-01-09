3cf2bae2a4f4537fb31bdec9e13a4044e601f54b
Niklas Baudy <niklas.baudy@vanniktech.de>
2019-01-09 19:39:27 +0100

    Add MockRxBilling in a separate module for easier testing. (#10)

---
+++ rxbilling-testing/src/main/kotlin/com/vanniktech/rxbilling/testing/MockRxBilling.kt
@@ -0,0 +1,51 @@
+package com.vanniktech.rxbilling.testing
+
+import com.vanniktech.rxbilling.Inventory
+import com.vanniktech.rxbilling.InventoryInApp
+import com.vanniktech.rxbilling.InventorySubscription
+import com.vanniktech.rxbilling.PurchaseResponse
+import com.vanniktech.rxbilling.PurchasedInApp
+import com.vanniktech.rxbilling.PurchasedSubscription
+import com.vanniktech.rxbilling.RxBilling
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+
+class MockRxBilling(
+  private val queryInAppPurchases: Observable<InventoryInApp> = Observable.never(),
+  private val querySubscriptions: Observable<InventorySubscription> = Observable.never(),
+  private val isBillingForInAppSupported: Completable = Completable.never(),
+  private val isBillingForSubscriptionsSupported: Completable = Completable.never(),
+  private val purchase: Single<PurchaseResponse> = Single.never(),
+  private val getPurchasedInApp: Observable<PurchasedInApp> = Observable.never(),
+  private val getPurchasedSubscriptions: Observable<PurchasedSubscription> = Observable.never(),
+  private val consumePurchase: Single<Int> = Single.never()
+) : RxBilling {
+  private var destroyed = false
+
+  override fun queryInAppPurchases(vararg skuIds: String) = returnIfNotDestroyed { queryInAppPurchases }
+
+  override fun querySubscriptions(vararg skuIds: String) = returnIfNotDestroyed { querySubscriptions }
+
+  override fun isBillingForInAppSupported() = returnIfNotDestroyed { isBillingForInAppSupported }
+
+  override fun isBillingForSubscriptionsSupported() = returnIfNotDestroyed { isBillingForSubscriptionsSupported }
+
+  override fun purchase(inventory: Inventory, developerPayload: String) = returnIfNotDestroyed { purchase }
+
+  override fun getPurchasedInApps() = returnIfNotDestroyed { getPurchasedInApp }
+
+  override fun getPurchasedSubscriptions() = returnIfNotDestroyed { getPurchasedSubscriptions }
+
+  override fun consumePurchase(purchasedInApp: PurchasedInApp) = returnIfNotDestroyed { consumePurchase }
+
+  override fun destroy() = returnIfNotDestroyed { destroyed = true }
+
+  private fun <T> returnIfNotDestroyed(body: () -> T): T {
+    if (destroyed) {
+      throw UnsupportedOperationException("RxBilling instance has been destroyed already")
+    }
+
+    return body.invoke()
+  }
+}
