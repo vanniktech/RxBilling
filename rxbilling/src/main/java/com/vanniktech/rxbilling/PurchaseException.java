f5c07f583f3f3b006bae6964e7e225f15804b74f
Niklas Baudy <niklas.baudy@vanniktech.de>
2021-10-06 00:07:56 +0200

    PurchaseException: Include sku.

--- rxbilling/src/main/java/com/vanniktech/rxbilling/PurchaseException.java
+++ rxbilling/src/main/java/com/vanniktech/rxbilling/PurchaseException.java
@@ -8,18 +8,21 @@ import static com.vanniktech.rxbilling.BillingResponseUtil.asDebugString;
 @SuppressWarnings("PMD.BeanMembersShouldSerialize") public final class PurchaseException extends RuntimeException {
   static final long serialVersionUID = 5285558498485989665L;
 
+  public final String sku;
   @BillingResponse public final int responseCode;
   @Nullable public final String debugMessage;
 
-  public PurchaseException(@BillingResponse final int responseCode) {
-    super("Error during purchase. ResponseCode: " + responseCode + " (" + asDebugString(responseCode) + ")");
+  public PurchaseException(final String sku, @BillingResponse final int responseCode) {
+    super("Error during purchase of \"" + sku + "\". ResponseCode: " + responseCode + " (" + asDebugString(responseCode) + ")");
     this.responseCode = responseCode;
+    this.sku = sku;
     debugMessage = null;
   }
 
-  public PurchaseException(@BillingResponse final int responseCode, @Nullable final String debugMessage) {
-    super("Error during purchase. ResponseCode " + responseCode + " (" + asDebugString(responseCode) + ") and message " + debugMessage);
+  public PurchaseException(final String sku, @BillingResponse final int responseCode, @Nullable final String debugMessage) {
+    super("Error during purchase of \"" + sku + "\". ResponseCode " + responseCode + " (" + asDebugString(responseCode) + ") and message " + debugMessage);
     this.responseCode = responseCode;
+    this.sku = sku;
     this.debugMessage = debugMessage;
   }
 }
