0740d302c6354c56c2e612128750a52bc7086692
Niklas Baudy <niklas.baudy@vanniktech.de>
2021-10-09 19:53:28 +0200

    Update dependencies.

--- rxbilling-google-play-library-v3/src/test/java/com/vanniktech/rxbilling/google/play/library/BillingResponseTest.kt
+++ rxbilling-google-play-library-v3/src/test/java/com/vanniktech/rxbilling/google/play/library/BillingResponseTest.kt
@@ -2,19 +2,19 @@ package com.vanniktech.rxbilling.google.play.library
 
 import com.android.billingclient.api.BillingClient.BillingResponseCode
 import com.vanniktech.rxbilling.RxBilling.BillingResponse as RxBillingResponse
-import org.assertj.core.api.Java6Assertions.assertThat
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class BillingResponseTest {
   @Test fun equality() {
-    assertThat(RxBillingResponse.OK).isEqualTo(BillingResponseCode.OK)
-    assertThat(RxBillingResponse.USER_CANCELED).isEqualTo(BillingResponseCode.USER_CANCELED)
-    assertThat(RxBillingResponse.SERVICE_UNAVAILABLE).isEqualTo(BillingResponseCode.SERVICE_UNAVAILABLE)
-    assertThat(RxBillingResponse.BILLING_UNAVAILABLE).isEqualTo(BillingResponseCode.BILLING_UNAVAILABLE)
-    assertThat(RxBillingResponse.ITEM_UNAVAILABLE).isEqualTo(BillingResponseCode.ITEM_UNAVAILABLE)
-    assertThat(RxBillingResponse.DEVELOPER_ERROR).isEqualTo(BillingResponseCode.DEVELOPER_ERROR)
-    assertThat(RxBillingResponse.ERROR).isEqualTo(BillingResponseCode.ERROR)
-    assertThat(RxBillingResponse.ITEM_ALREADY_OWNED).isEqualTo(BillingResponseCode.ITEM_ALREADY_OWNED)
-    assertThat(RxBillingResponse.ITEM_NOT_OWNED).isEqualTo(BillingResponseCode.ITEM_NOT_OWNED)
+    assertEquals(BillingResponseCode.OK, RxBillingResponse.OK)
+    assertEquals(BillingResponseCode.USER_CANCELED, RxBillingResponse.USER_CANCELED)
+    assertEquals(BillingResponseCode.SERVICE_UNAVAILABLE, RxBillingResponse.SERVICE_UNAVAILABLE)
+    assertEquals(BillingResponseCode.BILLING_UNAVAILABLE, RxBillingResponse.BILLING_UNAVAILABLE)
+    assertEquals(BillingResponseCode.ITEM_UNAVAILABLE, RxBillingResponse.ITEM_UNAVAILABLE)
+    assertEquals(BillingResponseCode.DEVELOPER_ERROR, RxBillingResponse.DEVELOPER_ERROR)
+    assertEquals(BillingResponseCode.ERROR, RxBillingResponse.ERROR)
+    assertEquals(BillingResponseCode.ITEM_ALREADY_OWNED, RxBillingResponse.ITEM_ALREADY_OWNED)
+    assertEquals(BillingResponseCode.ITEM_NOT_OWNED, RxBillingResponse.ITEM_NOT_OWNED)
   }
 }
