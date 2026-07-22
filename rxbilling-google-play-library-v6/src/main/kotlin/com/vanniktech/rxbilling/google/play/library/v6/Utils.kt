17484dce91397b1cc874576f7bd346ab1ba23d02
Niklas Baudy <niklas.baudy@vanniktech.de>
2026-07-21 19:20:16 -0500

    Breaking: Stop support of com.vanniktech:rxbilling-google-play-library-v6

--- rxbilling-google-play-library-v6/src/main/kotlin/com/vanniktech/rxbilling/google/play/library/v6/Utils.kt
+++
@@ -1,9 +0,0 @@
-@file:JvmName("Utils")
-
-package com.vanniktech.rxbilling.google.play.library.v6
-
-import java.math.BigDecimal
-import java.math.RoundingMode
-
-internal fun Long.microsAsBigDecimal() = BigDecimal.valueOf(this, 6)
-  .setScale(2, RoundingMode.HALF_UP)
